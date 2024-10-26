913de813bd9ae7c2b8e69a7d7597b67551603d8f
MGIC12 <mager.infante@gmail.com>
2024-10-25 22:49:06 -0300

    Register completado, login en construcción...

--- app.js
+++ app.js
@@ -44,6 +44,62 @@ app.get("/register", (req, res) => {
   res.render("register");
 });
 
+// Registración
+app.post("/register", async (req, res) => {
+  const user = req.body.user;
+  const name = req.body.name;
+  const rol = req.body.rol;
+  const pass = req.body.pass;
+  let passwordHash = await bcryptjs.hash(pass, 8);
+  connection.query(
+    "INSERT INTO users SET ?",
+    {
+      user: user,
+      name: name,
+      rol: rol,
+      pass: passwordHash,
+    },
+    async (error, results) => {
+      if (error) {
+        console.log(error);
+      } else {
+        res.render("register", {
+          alert: true,
+          alertTitle: "Registration",
+          alertMessage: "Registration successfull",
+          alertIcon: "success",
+          showConfirmButton: false,
+          timer: 1500,
+          ruta: "",
+        });
+      }
+    }
+  );
+});
+
+// Autenticación
+app.post("/auth", async (req, res) => {
+  const user = req.body.user;
+  const pass = req.body.pass;
+  let passwordHash = await bcryptjs.hash(pass, 8);
+  if (user && pass) {
+    connection.query(
+      "SELECT * FROM users WHERE user = ?",
+      [user],
+      async (error, results) => {
+        if (
+          results.length == 0 ||
+          !(await bcryptjs.compare(pass, results[0].pass))
+        ) {
+          res.send("El usuario o la contraseña son incorrectos");
+        } else {
+          res.send("Login correcto");
+        }
+      }
+    );
+  }
+});
+
 app.listen(3000, (req, res) => {
   console.log("SERVER RUNNING IN http://localhost:3000");
 });
